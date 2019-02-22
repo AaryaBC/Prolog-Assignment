b25289700c1402ea0c2531aa8cddc4ccaf50bf99
Aarya BC <aaryabc49@gmail.com>
2019-02-22 12:01:15 -0500

    question 1, all test cases passed

--- prolog.pl
+++ prolog.pl
@@ -2,8 +2,8 @@ sum-up-numbers-simple([], 0).
 
 sum-up-numbers-simple([HEAD|TAIL], TOTAL) :- 
     number(HEAD),
-    sum-up-numbers-simple(TAIL, REMAINDER),
-    TOTAL is HEAD + REMAINDER.
+    sum-up-numbers-simple(TAIL, REMAINING),
+    TOTAL is HEAD + REMAINING.
 
 sum-up-numbers-simple([_|TAIL], TOTAL) :-
     sum-up-numbers-simple(TAIL, TOTAL).
