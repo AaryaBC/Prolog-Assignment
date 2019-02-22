e08cd86798e93d90f15d0d9a6186858fd32a1f73
Aarya BC <aaryabc49@gmail.com>
2019-02-22 18:19:59 -0500

    Question 3 completed

--- prolog.pl
+++ prolog.pl
@@ -14,7 +14,7 @@ sum-up-numbers-general([HEAD|TAIL], TARGET) :-
 sum-up-numbers-general([_|TAIL], TARGET) :-
     sum-up-numbers-general(TAIL, TARGET).
 
-;;;;;;;
+% DONE
 
 sum-up-numbers-simple([], 0).
 
@@ -25,3 +25,59 @@ sum-up-numbers-simple([HEAD|TAIL], TOTAL) :-
 
 sum-up-numbers-simple([_|TAIL], TOTAL) :-
     sum-up-numbers-simple(TAIL, TOTAL).
+
+%DONE
+
+min-above-min([SINGLENUM], [], SINGLENUM) :-
+    number(SINGLENUM).
+
+min-above-min(LIST1, LIST2, TARGET) :-
+    length(LIST2, 0),
+    min-val-in-list(LIST1, TARGET).
+
+min-above-min([HEAD|TAIL], LIST2, HEAD) :-
+    number(HEAD),
+    min-val-in-list(LIST2, MINLIST),
+    MINLIST < HEAD,
+    larger-num-than-current-given(TAIL, MINLIST, HEAD, HEAD).
+
+min-above-min([_|TAIL], LIST2, TARGET) :-
+    min-above-min(TAIL, LIST2, TARGET).
+
+
+%HELPER TO FIND THE MINIMUM IN A LIST
+min-val-in-list([SINGLENUM], SINGLENUM) :-
+    number(SINGLENUM).
+
+min-val-in-list([HEAD, TEMP|TAIL], MINIMUM) :-
+    number(HEAD),
+    number(TEMP),
+    HEAD =< TEMP,
+    min-val-in-list([HEAD|TAIL], MINIMUM).
+
+min-val-in-list([HEAD, TEMP|TAIL], MINIMUM) :-
+    number(HEAD),
+    number(TEMP),
+    TEMP < HEAD,
+    min-val-in-list([TEMP|TAIL], MINIMUM).
+
+min-val-in-list([HEAD, _|TAIL], MINIMUM) :-
+    number(HEAD),
+    min-val-in-list([HEAD|TAIL], MINIMUM).
+
+min-val-in-list([_|TAIL], MINIMUM) :-
+    min-val-in-list(TAIL, MINIMUM).
+
+
+%HELPER TO FIND THE CURRENT BIGGEST NUMBER IS A LIST
+%FINDS THE NUMBER BIGGER THAN THE CURRENT PROVIDED NUMBER
+larger-num-than-current-given([], _, VAL, VAL). 
+
+larger-num-than-current-given([HEAD|TAIL], CURRENT, VALUE, HEAD) :-
+    number(HEAD),
+    HEAD > CURRENT,
+    HEAD < VALUE,
+    larger-num-than-current-given(TAIL, CURRENT, HEAD, HEAD).
+
+larger-num-than-current-given([_|TAIL], CURRENT, VALUE, TARGET) :-
+    larger-num-than-current-given(TAIL, CURRENT, VALUE, TARGET).
