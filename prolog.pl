b425ec6a551cc0238f897d9783702a7971cad87c
Aarya BC <aaryabc49@gmail.com>
2019-02-22 14:26:16 -0500

    question 2 completed

--- prolog.pl
+++ prolog.pl
@@ -1,3 +1,21 @@
+sum-up-numbers-general([], 0).
+
+sum-up-numbers-general([HEAD|TAIL], TARGET) :-
+    is_list(HEAD),
+    sum-up-numbers-general(TAIL, REMAINING),
+    sum-up-numbers-general(HEAD, TOTALHEAD),
+    TARGET is REMAINING + TOTALHEAD.
+
+sum-up-numbers-general([HEAD|TAIL], TARGET) :-
+    number(HEAD),
+    sum-up-numbers-general(TAIL, REMAINING),
+    TARGET is REMAINING + HEAD.
+
+sum-up-numbers-general([_|TAIL], TARGET) :-
+    sum-up-numbers-general(TAIL, TARGET).
+
+;;;;;;;
+
 sum-up-numbers-simple([], 0).
 
 sum-up-numbers-simple([HEAD|TAIL], TOTAL) :- 
