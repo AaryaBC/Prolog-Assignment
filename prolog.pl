54a0327d39e00a75a7e384aa02410c22ba40b112
Aarya BC <aaryabc49@gmail.com>
2019-02-22 20:17:32 -0500

    Update part 4, add comments

--- prolog.pl
+++ prolog.pl
@@ -1,46 +1,73 @@
+/**
+QUESTION 2
+**/
+
+%For list with no element which results in a sum of 0
 sum-up-numbers-general([], 0).
 
+%Case if the first element of the list is a list, we recursively go inside the nested list and apply this same function again
 sum-up-numbers-general([HEAD|TAIL], TARGET) :-
     is_list(HEAD),
     sum-up-numbers-general(TAIL, REMAINING),
     sum-up-numbers-general(HEAD, TOTALHEAD),
     TARGET is REMAINING + TOTALHEAD.
 
+%Case if the first element of the list is a number i.e valid case
 sum-up-numbers-general([HEAD|TAIL], TARGET) :-
     number(HEAD),
     sum-up-numbers-general(TAIL, REMAINING),
     TARGET is REMAINING + HEAD.
 
+%Case if first element of the list not a number
 sum-up-numbers-general([_|TAIL], TARGET) :-
     sum-up-numbers-general(TAIL, TARGET).
 
-% DONE
 
+/****************************************************************************************************************************/
+
+/**
+QUESTION 1
+**/
+
+%For list with no element which results in a sum of 0
 sum-up-numbers-simple([], 0).
 
+%Case if the first element of the list is a number i.e valid case
 sum-up-numbers-simple([HEAD|TAIL], TOTAL) :- 
     number(HEAD),
     sum-up-numbers-simple(TAIL, REMAINING),
     TOTAL is HEAD + REMAINING.
 
+%Case if first element of the list not a number
 sum-up-numbers-simple([_|TAIL], TOTAL) :-
     sum-up-numbers-simple(TAIL, TOTAL).
 
-%DONE
 
+/****************************************************************************************************************************/
+
+/**
+QUESTION 3
+**/
+
+%Case if the first list has only one element
 min-above-min([SINGLENUM], [], SINGLENUM) :-
     number(SINGLENUM).
 
+%Case when the second list is empty
 min-above-min(LIST1, LIST2, TARGET) :-
-    length(LIST2, 0),
-    min-val-in-list(LIST1, TARGET).
+    length(LIST2, 0), %Check for the second list being empty
+    min-val-in-list(LIST1, TARGET). %return the minimum element of list1 using helper function
 
+%Case when the first element is a number
+%also, the first element is greater than the minimum value we have
+%makes call to a function to find the number that is larger than the current number
 min-above-min([HEAD|TAIL], LIST2, HEAD) :-
     number(HEAD),
     min-val-in-list(LIST2, MINLIST),
     MINLIST < HEAD,
     larger-num-than-current-given(TAIL, MINLIST, HEAD, HEAD).
 
+%Other cases
 min-above-min([_|TAIL], LIST2, TARGET) :-
     min-above-min(TAIL, LIST2, TARGET).
 
@@ -82,8 +109,14 @@ larger-num-than-current-given([HEAD|TAIL], CURRENT, VALUE, HEAD) :-
 larger-num-than-current-given([_|TAIL], CURRENT, VALUE, TARGET) :-
     larger-num-than-current-given(TAIL, CURRENT, VALUE, TARGET).
 
-%DONE
 
+/****************************************************************************************************************************/
+
+/**
+QUESTION 4
+**/
+
+%Base case
 common-unique-elements([], _, []).
 
 common-unique-elements([HEAD|TAIL], LIST2, [HEAD|TAIL2]) :- 
